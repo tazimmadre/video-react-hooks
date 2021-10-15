b2516dd938dbc51463efd911c8eaed537b370105
Tazim <tazimmadre5041@gmail.com>
2021-10-15 12:02:24 +0530

    autcom

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-const SearchBar=({onFormSubmit})=>{
-  const [term,setTerm]=useState('');
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
   const onInputChange = (event) => {
     setTerm(event.target.value);
   };
@@ -16,17 +16,26 @@ const SearchBar=({onFormSubmit})=>{
     >
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label style={{ color: "white", fontSize: "24px" }}>Youtube</label>
-          <input
-          style={{ borderRadius: "10px" }}
- 
-            type="text"
-            value={term}
-            onChange={onInputChange}
-          />
+          <div class="ui search focus">
+            <div class="ui icon input">
+              <label style={{ color: "white", fontSize: "24px" }}>
+                Youtube
+              </label>
+              <input
+                class="search"
+                autocomplete="off"
+                style={{ borderRadius: "10px" }}
+                type="text"
+                value={term}
+                onChange={onInputChange}
+              />
+              <i class="search icon"></i>
+            </div>
+            <div class="results"></div>
+          </div>
         </div>
       </form>
     </div>
   );
-}
+};
 export default SearchBar;
